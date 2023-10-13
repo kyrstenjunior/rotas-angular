d82c1d4fc539e6ce7ee2757de139c99ef0190b7d
kyrsten-note <kyrstenjr@gmail.com>
2023-10-13 01:46:03 -0300

    finalização do modulo de services

--- src/app/components/boxes/boxes.component.ts
+++ src/app/components/boxes/boxes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
@@ -8,15 +9,42 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 })
 
 export class BoxesComponent {
-  name: string = "Bulbasaur";
-  attributesTypes: string[] = ['Earth', 'Poison'];
+  // Objeto inicializado e tipado
+  pokemon: PokemonData = {
+    id: 0,
+    name: '',
+    sprites: {
+      other: {
+        dream_world: {
+          front_default: ''
+        }
+      }
+    },
+    types: []
+  };
+  
 
-  constructor(private service:PokemonService) {
-    this.service.getPokemon("pikachu").subscribe(
+  constructor(private service:PokemonService) {}
+
+  ngOnInit(): void {
+    this.getPokemon('pikachu');
+  }
+
+  getPokemon(searchName: string){
+    this.service.getPokemon(searchName).subscribe(
       {
-        next: (res) => console.log(res),
+        next: (res) => {
+          // Traz da API só os itens que vou usar. Usando o models (pokemonData)
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+        },
         error: (err) => console.log(err)
       }
     );
   }
+
 }
--- src/app/models/pokemonData.ts
+++ src/app/models/pokemonData.ts
@@ -1,4 +1,19 @@
 export type PokemonData = {
   name: string,
-  id: number
+  id: number,
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string
+      }
+    }
+  },
+  types: {
+    // da forma que está na API
+    slot: number,
+    type: {
+      name: string,
+      url: string
+    }
+  }[]
 }
